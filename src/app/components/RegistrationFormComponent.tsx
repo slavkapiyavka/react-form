c7dd8517206a6573d9669187bc02883c7daad9c7
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2024-04-30 20:09:37 +0200

    fix: wrap Select and DatePicker into Controller

--- src/app/components/RegistrationFormComponent.tsx
+++ src/app/components/RegistrationFormComponent.tsx
@@ -2,7 +2,8 @@
 
 import React from 'react'
 import type { FormProps } from 'antd'
-import { Button, Form, Input, Select } from 'antd'
+import { Button, Col, DatePicker, Form, Input, Row, Select } from 'antd'
+import { useForm, Controller } from 'react-hook-form'
 
 type FieldType = {
   username: string,
@@ -15,6 +16,8 @@ type FieldType = {
 }
 
 const RegistrationForm: React.FC = () => {
+  const { register, control } = useForm<FieldType>()
+
   const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
     console.log('Success:', values);
   };
@@ -26,8 +29,8 @@ const RegistrationForm: React.FC = () => {
   const prefixSelector = (
     <Form.Item name="prefix" noStyle>
       <Select style={{ width: 80 }}>
-        <Option value="7">+7</Option>
-        <Option value="375">+375</Option>
+        <Select.Option value="7">+7</Select.Option>
+        <Select.Option value="375">+375</Select.Option>
       </Select>
     </Form.Item>
   );
@@ -39,13 +42,15 @@ const RegistrationForm: React.FC = () => {
       onFinishFailed={onFinishFailed}
       autoComplete="off"
       scrollToFirstError
+      initialValues={{ prefix: '7', gender: ''}}
+      style={{ maxWidth: 600 }}
     >
       <Form.Item<FieldType>
         label="Username (имя пользователя)"
         name="username"
         rules={[{ required: true, message: 'пожалуйста, введите ваш username' }]}
       >
-        <Input placeholder="Ваш username" />
+        <Input placeholder="Ваш username" {...register('username', { required: true })}/>
       </Form.Item>
 
       <Form.Item<FieldType>
@@ -53,7 +58,7 @@ const RegistrationForm: React.FC = () => {
         name="email"
         rules={[{ required: true, message: 'пожалуйста, введите ваш email', type: 'email' }]}
       >
-        <Input placeholder="Ваш email" />
+        <Input placeholder="Ваш email" {...register('email', { required: true })} />
       </Form.Item>
       
       <Form.Item<FieldType>
@@ -62,7 +67,7 @@ const RegistrationForm: React.FC = () => {
         rules={[{ required: true, message: 'пожалуйста, введите пароль' }]}
         hasFeedback
       >
-        <Input.Password placeholder="Ваш новый пароль" />
+        <Input.Password placeholder="Ваш новый пароль" {...register('password', { required: true })} />
       </Form.Item>
       
       <Form.Item<FieldType>
@@ -78,27 +83,46 @@ const RegistrationForm: React.FC = () => {
           },
         }),]}
       >
-        <Input.Password placeholder="Повторите ваш новый пароль" />
+        <Input.Password placeholder="Повторите ваш новый пароль" {...register('passwordRepeat', { required: true })} />
       </Form.Item>
 
-      <Form.Item<FieldType>
-        label="Дата рождения"
-        name="birthdate"
-        rules={[{ required: true, message: 'пожалуйста, введите вашу дату рождения', type: 'date' }]}
-      >
-        <Input placeholder="Ваша дата рождения" />
-      </Form.Item>
+      <Row gutter={8}>
+        <Col span={12}>
+          <Form.Item<FieldType>
+            label="Дата рождения"
+            name="birthdate"
+            rules={[{ required: true, message: 'пожалуйста, введите вашу дату рождения' }]}
+          >
+            <Controller
+              control={control}
+              name="birthdate"
+              render={({ field }) => (
+                <DatePicker placeholder="Ваша дата рождения" {...field} style={{ width: '100%' }}/>
+              )}
+            />
+          </Form.Item>
+        </Col>
 
-      <Form.Item
-        label="Пол"
-        name="gender"
-        rules={[{ required: true, message: 'пожалуйста, укажите пол' }]}
-      >
-        <Select placeholder="Укажите пол" defaultValue="male">
-          <Option value="male">Мужской</Option>
-          <Option value="female">Женский</Option>
-        </Select>
-      </Form.Item>
+        <Col span={12}>
+          <Form.Item<FieldType>
+            label="Пол"
+            name="gender"
+            rules={[{ required: true, message: 'пожалуйста, укажите пол' }]}
+          >
+            <Controller
+              control={control}
+              name="gender"
+              render={({ field }) => (
+                <Select placeholder="Укажите пол" {...field}>
+                  <Select.Option value="male">Мужской</Select.Option>
+                  <Select.Option value="female">Женский</Select.Option>
+                </Select>
+              )}
+            />
+          </Form.Item>
+        </Col>
+      </Row>
+
 
       <Form.Item<FieldType>
         label="Номер телефона"
