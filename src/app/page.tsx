e9306b72ca079c60f137d3e06104a79181b3e154
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2024-05-01 20:23:06 +0200

    fix: birthdate in modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,12 @@ import { RegistrationFormData } from './shared/interfaces';
 export default function Home() {
   const [api, contextHolder] = notification.useNotification()
   const data = ({ username, email, phoneNumber, birthdate }: RegistrationFormData) => {
-    const birthdateDay = new Date(birthdate).getDay()
-    const birthdateMonth = new Date(birthdate).getMonth()
+    const birthdateDay = new Date(birthdate).getDate()
+    const birthdateMonth = new Date(birthdate).getMonth() + 1
     const birthdateYear = new Date(birthdate).getFullYear()
+
+    console.log('birthdate: ', birthdate);
+    
 
     api.open({
       message: `Пользователь ${username} успешно зарегистрирован`,
